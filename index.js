30f6bbfcd0ad225bc3508106c3f4bca3ee01f36b
Maksim Chemerisuk <chemerisuk@users.noreply.github.com>
2015-12-23 17:35:58 +0300

    Add landscape xxhdpi and xxxhdpi splash for Android

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ var getPlatforms = function (projectName) {
             { name : 'drawable-land-mdpi/screen.png',  width : 480, height: 320 },
             { name : 'drawable-land-hdpi/screen.png',  width : 800, height: 480 },
             { name : 'drawable-land-xhdpi/screen.png', width : 1280, height: 720 },
+            { name : 'drawable-land-xxhdpi/screen.png', width : 1600, height: 960 },
+            { name : 'drawable-land-xxxhdpi/screen.png', width : 1920, height: 1280 },
             { name : 'drawable-port-ldpi/screen.png',  width : 200, height: 320 },
             { name : 'drawable-port-mdpi/screen.png',  width : 320, height: 480 },
             { name : 'drawable-port-hdpi/screen.png',  width : 480, height: 800 },
